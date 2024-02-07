15a856adc301e3f623377837e896438a2c68bcb8
0xVedansh <vedansh83138@gmail.com>
2024-02-07 22:45:15 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ const TabComponent = () => {
     const fetchTabContent = async () => {
       // Assuming tabContentData is fetched from an API
       const tabContentData = [
-        { title: 'TOMMY', content: 'Full Stack Web Developer' },
-        { title: 'BIGDROP', content: 'Front-End Engineer' },
-        { title: 'CUKER', content: 'Engineering Intern' },
+        { title: 'TOMMY', content: 'Full Stack Web Developer', time: 'December 2015 - Present' },
+        { title: 'BIGDROP', content: 'Front-End Engineer', time: 'May 2015 - December 2015' },
+        { title: 'CUKER', content: 'Engineering Intern', time: 'May 2014 - September 2015' },
       ];
       setTabContent(tabContentData);
     };
@@ -42,7 +42,9 @@ const TabComponent = () => {
         <div className="tab-content">
           {tabContent.length > 0 && (
             <>
-              <p>{tabContent[selectedTab].content}</p>
+              <p className="text-edit">{tabContent[selectedTab].content}</p>
+              <p><span className="text-edit1">{tabContent[selectedTab].title}</span></p>
+              <p>{tabContent[selectedTab].time}</p>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget magna nec lorem ullamcorper commodo eget eget quam. Integer non quam a eros fermentum vulputate. Donec euismod tellus at lacus congue, vel sodales nisi accumsan. Donec viverra mi a turpis facilisis lobortis. Integer non varius sapien, vel lobortis elit. Donec scelerisque ex eget nisi fermentum, id suscipit dui faucibus. Sed non dui eget lacus bibendum auctor. Proin vitae nisi at odio vehicula consectetur. Vivamus sed velit quis quam vehicula scelerisque eu eget dui. Nam non mi at eros elementum tristique a non ante. Vestibulum non metus vel nulla malesuada egestas. In laoreet hendrerit lectus, quis posuere mauris vehicula non. Ut fringilla, leo a consequat efficitur, lectus lectus consectetur eros, sed tincidunt leo nulla nec eros.</p>
               <div className="more-info-container">
                 <button className="more-info-btn">More Info</button>
